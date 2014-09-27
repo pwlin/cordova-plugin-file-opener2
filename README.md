4961f3392d26a140d9e2d6ce6664e8631932036e
pwlin <pwlin05@gmail.com>
2014-09-27 20:44:44 +0200

    Added "uninstall" and "appIsInstalled" for Android. Updated the README file.

--- README.md
+++ README.md
@@ -21,31 +21,27 @@ And search for `io.github.pwlin.cordova.plugins.fileopener2`
 Usage
 ------
 Open an APK install dialog:
-    
-    <script>
-        cordova.plugins.fileOpener2.open(
-            '/sdcard/Download/gmail.apk', 
-            'application/vnd.android.package-archive'
-        );
-    </script>
+
+    cordova.plugins.fileOpener2.open(
+        '/sdcard/Download/gmail.apk', 
+        'application/vnd.android.package-archive'
+    );
     
 Open a PDF document with the default PDF reader and optional callback object:
 
-    <script>
-        cordova.plugins.fileOpener2.open(
-    	    '/sdcard/Download/starwars.pdf', 
-    	    'application/pdf', 
-    	    { 
-    		    error : function(errorObj) { 
-    			    alert('Error status: ' + errorObj.status + ' - Error message: ' + errorObj.message); 
-    		    },
-    		    success : function () {
-    			    alert('file opened successfully'); 				
-    		    }
-    	    }
-        );
-    </script>
-    
+    cordova.plugins.fileOpener2.open(
+        '/sdcard/Download/starwars.pdf', // You can also use a Cordova-style file uri: cdvfile://localhost/persistent/Download/starwars.pdf
+        'application/pdf', 
+        { 
+            error : function(e) { 
+                console.log('Error status: ' + e.status + ' - Error message: ' + e.message);
+            },
+            success : function () {
+                console.log('file opened successfully'); 				
+            }
+        }
+    );
+
 Notes
 ------
 
@@ -54,3 +50,56 @@ Notes
 
 - [It is reported](https://github.com/pwlin/cordova-plugin-file-opener2/issues/2#issuecomment-41295793) that in iOS, you might need to remove `<preference name="iosPersistentFileLocation" value="Library" />` from your `config.xml`
 
+
+Additional Android Functions
+-----------------------------
+####The following functions are available in Android platform
+
+###.uninstall(_packageId, callbackContext_)
+Uninstall a package with its id.
+
+    cordova.plugins.fileOpener2.uninstall('com.zynga.FarmVille2CountryEscape', {
+        error : function(e) {
+            console.log('Error status: ' + e.status + ' - Error message: ' + e.message);    
+        },
+        success : function() {
+            console.log('Uninstall intent activity started.');
+        }
+    });
+
+###.appIsInstalled(_packageId, callbackContext_)
+Check if an app is already installed.
+
+    cordova.plugins.fileOpener2.isAppInstalled('com.adobe.reader', {
+        success : function(res) {
+            if (res.status === 0) {
+                console.log('Adobe Reader is not installed.');
+            } else {
+                console.log('Adobe Reader is installed.')
+            }
+        }
+    });
+
+
+LICENSE
+--------
+The MIT License (MIT)
+
+Copyright (c) 2013 pwlin - pwlin05@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- src/android/io/github/pwlin/cordova/plugins/fileopener2/FileOpener2.java
+++ src/android/io/github/pwlin/cordova/plugins/fileopener2/FileOpener2.java
@@ -1,13 +1,38 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013 pwlin - pwlin05@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 package io.github.pwlin.cordova.plugins.fileopener2;
 
 import java.io.File;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 //import android.util.Log;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
@@ -27,97 +52,102 @@ public class FileOpener2 extends CordovaPlugin {
 	 * @return boolean.
 	 */
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-
 		if (action.equals("open")) {
-
-			try {
-
-				return this._open(args.getString(0), args.getString(1), callbackContext);
-
-			} catch (JSONException e) {
-
-				JSONObject errorObj = new JSONObject();
-				errorObj.put("status", PluginResult.Status.JSON_EXCEPTION.ordinal());
-				errorObj.put("message", e.getMessage());
-				callbackContext.error(errorObj);
-				return false;
+			this._open(args.getString(0), args.getString(1), callbackContext);
+		} 
+		else if (action.equals("uninstall")) {
+			this._uninstall(args.getString(0), callbackContext);
+		}
+		else if (action.equals("appIsInstalled")) {
+			JSONObject successObj = new JSONObject();
+			if (this._appIsInstalled(args.getString(0))) {
+				successObj.put("status", PluginResult.Status.OK.ordinal());
+				successObj.put("message", "Installed");
 			}
-
-		} else {
-
+			else {
+				successObj.put("status", PluginResult.Status.NO_RESULT.ordinal());
+				successObj.put("message", "Not installed");
+			}
+			callbackContext.success(successObj);
+		}
+		else {
 			JSONObject errorObj = new JSONObject();
 			errorObj.put("status", PluginResult.Status.INVALID_ACTION.ordinal());
 			errorObj.put("message", "Invalid action");
 			callbackContext.error(errorObj);
-			return false;
-
 		}
-
+		return true;
 	}
 
-	/**
-	 * Identifies if action to be executed returns a value and should be run
-	 * synchronously.
-	 * 
-	 * @param action
-	 *            The action to execute
-	 * @return T=returns value
-	 */
-	public boolean isSynch(String action) {
-		return false;
-	}
-
-	/**
-	 * Called by AccelBroker when listener is to be shut down. Stop listener.
-	 */
-	public void onDestroy() {
-	}
-
-	private boolean _open(String fileArg, String contentType, CallbackContext callbackContext) throws JSONException {
+	private void _open(String fileArg, String contentType, CallbackContext callbackContext) throws JSONException {
 		String fileName = "";
 		try {
-	        CordovaResourceApi resourceApi = webView.getResourceApi();
-	        Uri fileUri = resourceApi.remapUri(Uri.parse(fileArg));
-	        fileName = org.apache.cordova.FileHelper.stripFileProtocol(fileUri.toString());
+			CordovaResourceApi resourceApi = webView.getResourceApi();
+			Uri fileUri = resourceApi.remapUri(Uri.parse(fileArg));
+			fileName = this.stripFileProtocol(fileUri.toString());
 		} catch (Exception e) {
 			fileName = fileArg;
-		}		
-        
+		}
 		File file = new File(fileName);
-
 		if (file.exists()) {
-
 			try {
-
 				Uri path = Uri.fromFile(file);
 				Intent intent = new Intent(Intent.ACTION_VIEW);
 				intent.setDataAndType(path, contentType);
 				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 				/*
-				 * @see  http://stackoverflow.com/questions/14321376/open-an-activity-from-a-cordovaplugin
+				 * @see
+				 * http://stackoverflow.com/questions/14321376/open-an-activity-from-a-cordovaplugin
 				 */
-				cordova.getActivity().startActivity(intent/*Intent.createChooser(intent,"Open File in...")*/);
+				cordova.getActivity().startActivity(intent);
+				//cordova.getActivity().startActivity(Intent.createChooser(intent,"Open File in..."));
 				callbackContext.success();
-				return true;
-
 			} catch (android.content.ActivityNotFoundException e) {
-
 				JSONObject errorObj = new JSONObject();
 				errorObj.put("status", PluginResult.Status.ERROR.ordinal());
 				errorObj.put("message", "Activity not found: " + e.getMessage());
 				callbackContext.error(errorObj);
-				return false;
 			}
-
 		} else {
-
 			JSONObject errorObj = new JSONObject();
 			errorObj.put("status", PluginResult.Status.ERROR.ordinal());
 			errorObj.put("message", "File not found");
 			callbackContext.error(errorObj);
-			return false;
-
 		}
+	}
+	
+	private void _uninstall(String packageId, CallbackContext callbackContext) throws JSONException {
+		if (this._appIsInstalled(packageId)) {
+			Intent intent = new Intent(Intent.ACTION_UNINSTALL_PACKAGE);
+			intent.setData(Uri.parse("package:" + packageId));
+			cordova.getActivity().startActivity(intent);
+			callbackContext.success();
+		}
+		else {
+			JSONObject errorObj = new JSONObject();
+			errorObj.put("status", PluginResult.Status.ERROR.ordinal());
+			errorObj.put("message", "This package is not installed");
+			callbackContext.error(errorObj);
+		}
+	}
+	
+	private boolean _appIsInstalled(String packageId) {
+		PackageManager pm = cordova.getActivity().getPackageManager();
+        boolean appInstalled = false;
+        try {
+            pm.getPackageInfo(packageId, PackageManager.GET_ACTIVITIES);
+            appInstalled = true;
+        } catch (PackageManager.NameNotFoundException e) {
+            appInstalled = false;
+        }
+        return appInstalled;
+	}
+
+	private String stripFileProtocol(String uriString) {
+		if (uriString.startsWith("file://")) {
+			uriString = uriString.substring(7);
+		}
+		return uriString;
 	}
 
 }
