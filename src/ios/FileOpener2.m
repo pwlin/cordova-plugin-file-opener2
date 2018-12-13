257482f323128f992c179c2d2f2f5f907d78eb88
shnist <aaron.jack.faber@gmail.com>
2018-12-13 10:24:42 +0000

    fix(): fixed compilation error in preview dialog position for ios

--- src/ios/FileOpener2.m
+++ src/ios/FileOpener2.m
@@ -44,7 +44,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 		NSArray *positionValues = command.arguments[3];
 		rect = CGRectMake(0,0,[[positionValues objectAtIndex:0] floatValue],[[positionValues objectAtIndex:1] floatValue]);
 	} else {
-		rect = CGRectMake(0, 0, cont.view.bounds.size.width, cont.view.bounds.size.height);
+		rect = CGRectMake(0, 0, 1000.0f, 150.0f);
 	}
 
 	CDVViewController* cont = (CDVViewController*)[super viewController];
